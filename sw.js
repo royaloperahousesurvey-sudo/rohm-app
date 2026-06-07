2a0d41b517eb521175baa8d5d56e24307905905b
royaloperahousesurvey-sudo <royaloperahousesurvey@gmail.com>
2026-06-07 19:05:25 +0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -3,7 +3,3 @@ self.addEventListener('activate', e => {
   e.waitUntil(caches.keys().then(ks => Promise.all(ks.map(k => caches.delete(k)))));
   clients.claim();
 });
-self.addEventListener('fetch', e => {
-  if (e.request.url.includes('/api/')) return;
-  e.respondWith(fetch(e.request));
-});
