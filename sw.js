8aa60f5b6a981e98ab84f1bd415c39ac23a46ac4
royaloperahousesurvey-sudo <royaloperahousesurvey@gmail.com>
2026-09-08 20:47:05 +0400

    Update sw.js
    Force update

--- sw.js
+++ sw.js
@@ -1,3 +1,4 @@
+// v3 - force update
 self.addEventListener('install', () => self.skipWaiting());
 self.addEventListener('activate', e => {
   e.waitUntil(caches.keys().then(ks => Promise.all(ks.map(k => caches.delete(k)))));
